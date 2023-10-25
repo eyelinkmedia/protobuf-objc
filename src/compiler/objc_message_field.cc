223d7cc15911ae0bf1df43ae60a2f26b86b032fb
Viacheslav Radchenko <viacheslav.radchenko@gmail.com>
2023-10-25 14:56:52 +0100

    Removed explicit init of ivar

--- src/compiler/objc_message_field.cc
+++ src/compiler/objc_message_field.cc
@@ -107,9 +107,7 @@ namespace google { namespace protobuf { namespace compiler { namespace objective
   }
 
   void MessageFieldGenerator::GenerateInitializationSource(io::Printer* printer) const {
-    printer->Print(variables_, "_$name$ = nil;\n");
   }
-
 
   void MessageFieldGenerator::GenerateBuilderMembersHeader(io::Printer* printer) const {
     printer->Print(variables_,
